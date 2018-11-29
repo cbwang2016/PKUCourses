4abd3250dc46fee43f2202667eefd8e0308052fd
cbwang2016 <ano>
2018-11-29 19:38:53 +0800

    File downloading feature enhancements

--- app/src/main/java/edu_cn/pku/course/activities/ContentViewActivity.java
+++ app/src/main/java/edu_cn/pku/course/activities/ContentViewActivity.java
@@ -1,17 +1,23 @@
 package edu_cn.pku.course.activities;
 
+import android.Manifest;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,9 +25,11 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.r0adkll.slidr.Slidr;
 
@@ -30,6 +38,13 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -42,6 +57,8 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 public class ContentViewActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, EasyPermissions.PermissionCallbacks {
 
+    private static final int WRITE_REQUEST_CODE = 300;
+
     private AttachedFilesListLoadingTask mLoadingTask = null;
     private SwipeRefreshLayout mSwipeContainer;
     private RecyclerView mRecyclerView;
@@ -50,6 +67,9 @@ public class ContentViewActivity extends AppCompatActivity implements SwipeRefre
     private TextView content_view_title, content_view_time;
     private WebView content_view_content_detail;
     private String course_id, content_id;
+
+    private String currentDownloadUrl = "";
+    private String currentDownloadFileName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -122,13 +142,193 @@ public class ContentViewActivity extends AppCompatActivity implements SwipeRefre
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
     public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
-        adapter.startDownload();
+        new DownloadFile().execute();
     }
 
     @Override
     public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
 
+    }
+
+    public void startDownload(AttachedFileItem item) {
+
+        currentDownloadUrl = item.getUrl();
+        currentDownloadFileName = item.getFileName();
+        if (item.isDownloaded()) {
+            openFile(Environment.getExternalStorageDirectory() + File.separator + Utils.downloadFolder + currentDownloadFileName);
+            return;
+        }
+        //check if app has permission to write to the external storage.
+        if (EasyPermissions.hasPermissions(ContentViewActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            //Get the URL entered
+            new DownloadFile().execute();
+
+        } else {
+            //If permission is not present request for the same.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                EasyPermissions.requestPermissions(ContentViewActivity.this, "PKU Courses是开源软件，绝不会滥用权限。请授权存储权限以下载文件。", WRITE_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+
+    /**
+     * Async Task to download file from URL
+     */
+    @SuppressLint("StaticFieldLeak")
+    private class DownloadFile extends AsyncTask<Void, String, String> {
+
+        private ProgressDialog progressDialog;
+        private String fileName;
+        private String folder;
+//        private boolean isDownloaded;
+
+        /**
+         * Before starting background thread
+         * Show Progress Bar Dialog
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            this.progressDialog = new ProgressDialog(ContentViewActivity.this);
+            this.progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            this.progressDialog.setCancelable(false);
+            this.progressDialog.show();
+        }
+
+        /**
+         * Downloading file in background thread
+         */
+        @Override
+        protected String doInBackground(Void... params) {
+            int count;
+            try {
+                SharedPreferences sharedPreferences = getSharedPreferences("login_info", Context.MODE_PRIVATE);
+                String session_id = sharedPreferences.getString("session_id", null);
+
+                URL url = new URL(currentDownloadUrl);
+                URLConnection connection = url.openConnection();
+                connection.setRequestProperty("Cookie", "session_id=" + session_id);
+                connection.connect();
+                // getting file length
+                int lengthOfFile = connection.getContentLength();
+
+                // input stream to read file - with 8k buffer
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+
+                fileName = currentDownloadFileName;
+
+                //External directory path to save file
+                folder = Environment.getExternalStorageDirectory() + File.separator + Utils.downloadFolder;
+
+                //Create androiddeft folder if it does not exist
+                File directory = new File(folder);
+
+                if (!directory.exists()) {
+                    if (!directory.mkdirs()) {
+                        throw new Exception("Error creating folder");
+                    }
+                }
+
+                // Output stream to write file
+                OutputStream output = new FileOutputStream(folder + fileName);
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    publishProgress("" + (int) ((total * 100) / lengthOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+                return folder + fileName;
+
+            } catch (Exception e) {
+                return Utils.errorPrefix + e.getMessage();
+            }
+        }
+
+        /**
+         * Updating progress bar
+         */
+        protected void onProgressUpdate(String... progress) {
+            // setting progress percentage
+            progressDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+
+        @Override
+        protected void onPostExecute(String message) {
+            // dismiss the dialog after the file was downloaded
+            this.progressDialog.dismiss();
+
+            if (message.startsWith(Utils.errorPrefix)) {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Toast.makeText(getApplicationContext(), "File downloaded at: " + message, Toast.LENGTH_LONG).show();
+            // Display File path after downloading
+//            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+            adapter.refreshList(); // 刷新已下载文件
+            openFile(message);
+        }
+    }
+
+    private void openFile(String filePath) {
+        final Uri data = FileProvider.getUriForFile(getApplicationContext(), "edu_cn.pku.course", new File(filePath));
+        grantUriPermission(getPackageName(), data, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        MimeTypeMap myMime = MimeTypeMap.getSingleton();
+        Intent newIntent = new Intent(Intent.ACTION_VIEW);
+        String mimeType = myMime.getMimeTypeFromExtension(fileExt(filePath).substring(1));
+        newIntent.setDataAndType(data, mimeType);
+        newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        newIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        try {
+            getApplicationContext().startActivity(newIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getApplicationContext(), "No handler for this type of file.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @NonNull
+    private String fileExt(String url) {
+        if (url.contains("?")) {
+            url = url.substring(0, url.indexOf("?"));
+        }
+        if (url.lastIndexOf(".") == -1) {
+            return ".";
+        } else {
+            String ext = url.substring(url.lastIndexOf(".") + 1);
+            if (ext.contains("%")) {
+                ext = ext.substring(0, ext.indexOf("%"));
+            }
+            if (ext.contains("/")) {
+                ext = ext.substring(0, ext.indexOf("/"));
+            }
+            return ext.toLowerCase();
+        }
     }
 
     @SuppressLint("StaticFieldLeak")
@@ -163,8 +363,9 @@ public class ContentViewActivity extends AppCompatActivity implements SwipeRefre
                         try {
                             content_view_time.setText(
                                     new SimpleDateFormat("yyyy年M月d日 H:m:s", Locale.CHINA)
-                                            .format(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz", Locale.US)
-                                                    .parse(contentNode.getAttribute("datemodified"))
+                                            .format(
+                                                    new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz", Locale.US)
+                                                            .parse(contentNode.getAttribute("datemodified"))
                                             )
                             );
                         } catch (ParseException e) {
@@ -180,8 +381,8 @@ public class ContentViewActivity extends AppCompatActivity implements SwipeRefre
                             content_view_content_detail.setVisibility(View.GONE);
                         }
 
+                        ArrayList<AttachedFileItem> item_list = new ArrayList<>();
                         if ((contentNode).getElementsByTagName("attachments").getLength() > 0) {
-                            ArrayList<AttachedFileItem> item_list = new ArrayList<>();
                             NodeList nList = (contentNode).getElementsByTagName("attachments").item(0).getChildNodes();
                             for (int i = 0; i < nList.getLength(); i++) {
                                 item_list.add(new AttachedFileItem((Element) nList.item(i)));
@@ -206,12 +407,21 @@ public class ContentViewActivity extends AppCompatActivity implements SwipeRefre
 
     public class AttachedFileItem {
         Element n;
+        boolean downloaded = false;
 
         AttachedFileItem(Element n) {
             this.n = n;
         }
 
-        public String getTitle() {
+        public void setDownloaded(boolean b) {
+            downloaded = b;
+        }
+
+        public boolean isDownloaded() {
+            return downloaded;
+        }
+
+        public String getFileName() {
             return n.getAttribute("name");
         }
 
@@ -219,7 +429,11 @@ public class ContentViewActivity extends AppCompatActivity implements SwipeRefre
             return Utils.readableFileSize(Integer.parseInt(n.getAttribute("filesize")));
         }
 
-        public String getUrl() {
+        public int getFileSizeInteger() {
+            return Integer.parseInt(n.getAttribute("filesize"));
+        }
+
+        private String getUrl() {
             return "http://course.pku.edu.cn" + n.getAttribute("uri");
         }
     }
--- app/src/main/java/edu_cn/pku/course/adapter/ContentViewAdapter.java
+++ app/src/main/java/edu_cn/pku/course/adapter/ContentViewAdapter.java
@@ -1,36 +1,21 @@
 package edu_cn.pku.course.adapter;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Build;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Environment;
 import android.support.annotation.NonNull;
-import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 
 import edu_cn.pku.course.Utils;
@@ -40,11 +25,7 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 public class ContentViewAdapter extends RecyclerView.Adapter<ContentViewAdapter.ContentViewItemHolder> {
 
-    private static final int WRITE_REQUEST_CODE = 300;
-    private static String currentDownloadUrl = "";
-    private static String currentDownloadFileName = "";
-
-    private Context mContext;
+    private ContentViewActivity mContext;
     private ArrayList<ContentViewActivity.AttachedFileItem> list;
 
     public ContentViewAdapter(ArrayList<ContentViewActivity.AttachedFileItem> list, ContentViewActivity context) {
@@ -60,7 +41,7 @@ public class ContentViewAdapter extends RecyclerView.Adapter<ContentViewAdapter.
 
     @Override
     public void onBindViewHolder(@NonNull final ContentViewItemHolder holder, int i) {
-        holder.content_view_file_name.setText(list.get(holder.getAdapterPosition()).getTitle());
+        holder.content_view_file_name.setText(list.get(holder.getAdapterPosition()).getFileName());
         holder.content_view_file_size.setText(list.get(holder.getAdapterPosition()).getFileSize());
 
         holder.card_view_item_content.setClickable(true);
@@ -68,33 +49,63 @@ public class ContentViewAdapter extends RecyclerView.Adapter<ContentViewAdapter.
             @Override
             public void onClick(View v) {
                 if (Utils.isSDCardPresent()) {
-
-                    currentDownloadUrl = list.get(holder.getAdapterPosition()).getUrl();
-                    currentDownloadFileName = list.get(holder.getAdapterPosition()).getTitle();
-                    //check if app has permission to write to the external storage.
-                    if (EasyPermissions.hasPermissions(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                        //Get the URL entered
-                        startDownload();
-
-                    } else {
-                        //If permission is not present request for the same.
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                            EasyPermissions.requestPermissions((Activity) mContext, "This app needs access to your file storage so that it can write files.", WRITE_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
-                        }
-                    }
+                    mContext.startDownload(list.get(holder.getAdapterPosition()));
                 } else {
                     Toast.makeText(mContext, "SD Card not found", Toast.LENGTH_LONG).show();
                 }
             }
         });
+
+        if (list.get(holder.getAdapterPosition()).isDownloaded()) {
+            System.out.println("is Downloaded!!!");
+            holder.card_view_item_content.setCardBackgroundColor(Color.parseColor("#66BB6A"));
+            holder.card_view_item_content.setLongClickable(true);
+            holder.card_view_item_content.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    new AlertDialog.Builder(mContext)
+                            .setMessage("您是否想删除此文件（" + list.get(holder.getAdapterPosition()).getFileName() + "）的本地缓存？")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    boolean tmp = new File(Environment.getExternalStorageDirectory() + File.separator + Utils.downloadFolder + list.get(holder.getAdapterPosition()).getFileName()).delete();
+                                    if (!tmp) {
+                                        new AlertDialog.Builder(mContext)
+                                                .setMessage("文件删除失败")
+                                                .setPositiveButton(android.R.string.yes, null).show();
+                                    }
+                                    list.get(holder.getAdapterPosition()).setDownloaded(false);
+                                    notifyDataSetChanged(); // bug here
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, null).show();
+                    return true;
+                }
+            });
+        }
     }
 
-    public void startDownload() {
-        new DownloadFile().execute();
+    public void refreshList() {
+        if (EasyPermissions.hasPermissions(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            for (ContentViewActivity.AttachedFileItem tmp : this.list) {
+                String filePath = Environment.getExternalStorageDirectory() + File.separator + Utils.downloadFolder + tmp.getFileName();
+                if (new File(filePath).exists()) {
+                    tmp.setDownloaded(true);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 
     public void updateList(ArrayList<ContentViewActivity.AttachedFileItem> item_list) {
         this.list = item_list;
+        if (EasyPermissions.hasPermissions(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            for (ContentViewActivity.AttachedFileItem tmp : this.list) {
+                String filePath = Environment.getExternalStorageDirectory() + File.separator + Utils.downloadFolder + tmp.getFileName();
+                if (new File(filePath).exists()) {
+                    tmp.setDownloaded(true);
+                }
+            }
+        }
         notifyDataSetChanged();
     }
 
@@ -115,146 +126,6 @@ public class ContentViewAdapter extends RecyclerView.Adapter<ContentViewAdapter.
             content_view_file_size = itemView.findViewById(R.id.content_view_file_size);
             card_view_item_content = itemView.findViewById(R.id.card_view_item_content);
             content_view_icon = itemView.findViewById(R.id.content_view_icon);
-        }
-    }
-
-    /**
-     * Async Task to download file from URL
-     */
-    @SuppressLint("StaticFieldLeak")
-    private class DownloadFile extends AsyncTask<Void, String, String> {
-
-        private ProgressDialog progressDialog;
-        private String fileName;
-        private String folder;
-//        private boolean isDownloaded;
-
-        /**
-         * Before starting background thread
-         * Show Progress Bar Dialog
-         */
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            this.progressDialog = new ProgressDialog(mContext);
-            this.progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-            this.progressDialog.setCancelable(false);
-            this.progressDialog.show();
-        }
-
-        /**
-         * Downloading file in background thread
-         */
-        @Override
-        protected String doInBackground(Void... params) {
-            int count;
-            try {
-                SharedPreferences sharedPreferences = mContext.getSharedPreferences("login_info", Context.MODE_PRIVATE);
-                String session_id = sharedPreferences.getString("session_id", null);
-
-                URL url = new URL(currentDownloadUrl);
-                URLConnection connection = url.openConnection();
-                connection.setRequestProperty("Cookie", "session_id=" + session_id);
-                connection.connect();
-                // getting file length
-                int lengthOfFile = connection.getContentLength();
-
-                // input stream to read file - with 8k buffer
-                InputStream input = new BufferedInputStream(url.openStream(), 8192);
-
-                fileName = currentDownloadFileName;
-
-                //External directory path to save file
-                folder = Environment.getExternalStorageDirectory() + File.separator + "Download/PKU_Courses/";
-
-                //Create androiddeft folder if it does not exist
-                File directory = new File(folder);
-
-                if (!directory.exists()) {
-                    if (!directory.mkdirs()) {
-                        throw new Exception("Error creating folder");
-                    }
-                }
-
-                // Output stream to write file
-                OutputStream output = new FileOutputStream(folder + fileName);
-
-                byte data[] = new byte[1024];
-
-                long total = 0;
-
-                while ((count = input.read(data)) != -1) {
-                    total += count;
-                    // publishing the progress....
-                    // After this onProgressUpdate will be called
-                    publishProgress("" + (int) ((total * 100) / lengthOfFile));
-
-                    // writing data to file
-                    output.write(data, 0, count);
-                }
-
-                // flushing output
-                output.flush();
-
-                // closing streams
-                output.close();
-                input.close();
-                return folder + fileName;
-
-            } catch (Exception e) {
-                return "Something went wrong" + e.getMessage();
-            }
-        }
-
-        /**
-         * Updating progress bar
-         */
-        protected void onProgressUpdate(String... progress) {
-            // setting progress percentage
-            progressDialog.setProgress(Integer.parseInt(progress[0]));
-        }
-
-
-        @Override
-        protected void onPostExecute(String message) {
-            // dismiss the dialog after the file was downloaded
-            this.progressDialog.dismiss();
-
-            // Display File path after downloading
-            Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
-
-            final Uri data = FileProvider.getUriForFile(mContext, "com.mypackage.myprovider", new File(message));
-            mContext.grantUriPermission(mContext.getPackageName(), data, Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            MimeTypeMap myMime = MimeTypeMap.getSingleton();
-            Intent newIntent = new Intent(Intent.ACTION_VIEW);
-            String mimeType = myMime.getMimeTypeFromExtension(fileExt(message).substring(1));
-            newIntent.setDataAndType(data, mimeType);
-            newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            newIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            try {
-                mContext.startActivity(newIntent);
-            } catch (ActivityNotFoundException e) {
-                Toast.makeText(mContext, "No handler for this type of file.", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    @NonNull
-    private String fileExt(String url) {
-        if (url.contains("?")) {
-            url = url.substring(0, url.indexOf("?"));
-        }
-        if (url.lastIndexOf(".") == -1) {
-            return ".";
-        } else {
-            String ext = url.substring(url.lastIndexOf(".") + 1);
-            if (ext.contains("%")) {
-                ext = ext.substring(0, ext.indexOf("%"));
-            }
-            if (ext.contains("/")) {
-                ext = ext.substring(0, ext.indexOf("/"));
-            }
-            return ext.toLowerCase();
         }
     }
 }
