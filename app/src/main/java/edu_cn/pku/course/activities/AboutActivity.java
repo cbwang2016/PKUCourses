ef88cd8b0dbf89f2c2170b87fb09776cac0eba68
Yinian0 <43735232+Yinian0@users.noreply.github.com>
2018-12-03 17:07:35 +0800

    美化about界面

--- app/src/main/java/edu_cn/pku/course/activities/AboutActivity.java
+++ app/src/main/java/edu_cn/pku/course/activities/AboutActivity.java
@@ -36,19 +36,19 @@ public class AboutActivity extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                switch (i){
+                switch (i) {
                     case 0:
                         // “问题反馈”：发送邮件
                         Intent j = new Intent(Intent.ACTION_SEND);
                         // j.setType("text/plain"); //模拟器请使用这行
                         j.setType("message/rfc822"); // 真机上使用这行
-                        j.putExtra(Intent.EXTRA_EMAIL, new String[] { "wcb@pku.edu.cn" });
+                        j.putExtra(Intent.EXTRA_EMAIL, new String[]{"wcb@pku.edu.cn"});
                         j.putExtra(Intent.EXTRA_SUBJECT, "您发现的问题及建议");
                         j.putExtra(Intent.EXTRA_TEXT, "我们很希望能得到您的建议！！！");
                         startActivity(Intent.createChooser(j, "Select email application."));
-                      break;
+                        break;
                     case 1:
-                        startActivity(new Intent(this,GitWebViewActivity.class));
+                        startActivity(new Intent(AboutActivity.this, GitWebViewActivity.class));
                         break;
                     case 2:
                         Toast.makeText(AboutActivity.this, "Click item" + 8, Toast.LENGTH_SHORT).show();
@@ -65,35 +65,38 @@ public class AboutActivity extends AppCompatActivity {
      * 初始化数据
      */
     private void getMenuList() {
-        mList.add(new AboutMenu(R.mipmap.icon_email_600,"问题反馈","Select email application to send an email to the developer.",""));
-        mList.add(new AboutMenu(R.mipmap.icon_github_logo_1024,"Github Page","Open source code in github.",""));
-        mList.add(new AboutMenu(R.mipmap.icon_function_768,"功能介绍","Features","还没想好怎么写"));
-        mList.add(new AboutMenu(R.mipmap.icon_member_980,"开发人员","Developer",""));
+        mList.add(new AboutMenu(R.mipmap.icon_email_600, "问题反馈", "Select email application to send an email to the developer.", ""));
+        mList.add(new AboutMenu(R.mipmap.icon_github_logo_1024, "Github Page", "Open source code in github.", ""));
+        mList.add(new AboutMenu(R.mipmap.icon_function_768, "功能介绍", "Features", "还没想好怎么写"));
+        mList.add(new AboutMenu(R.mipmap.icon_member_980, "开发人员", "Developer", ""));
     }
 
-    public class AboutMenu{
+    public class AboutMenu {
         private int imageId;
         private String menu;
         private String subMenu;
         private String content;
 
-        AboutMenu(int imageId, String menu, String subMenu, String content){
+        AboutMenu(int imageId, String menu, String subMenu, String content) {
             this.imageId = imageId;
             this.menu = menu;
             this.subMenu = subMenu;
             this.content = content;
         }
 
-        public String getMenu(){
+        public String getMenu() {
             return menu;
         }
-        public String getSubMenu(){
+
+        public String getSubMenu() {
             return subMenu;
         }
-        public int getImageId(){
+
+        public int getImageId() {
             return imageId;
         }
-        public String getContent(){
+
+        public String getContent() {
             return content;
         }
     }
