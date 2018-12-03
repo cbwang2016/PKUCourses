7a3bf7ae71ab94635acd05b3f3a0aee83584db89
ZhaoRibo <1294396795@qq.com>
2018-12-03 09:32:21 +0800

    About
    设置好了图标，完成了问题反馈打开邮件的操作。

--- app/src/main/java/edu_cn/pku/course/activities/AboutActivity.java
+++ app/src/main/java/edu_cn/pku/course/activities/AboutActivity.java
@@ -1,10 +1,12 @@
 package edu_cn.pku.course.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -14,16 +16,15 @@ import edu_cn.pku.course.adapter.AboutAdapter;
 
 
 public class AboutActivity extends AppCompatActivity {
-    private ListView listView;
-    private List<String> mList = new ArrayList<>();
+    private List<AboutMenu> mList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        initList();
+        getMenuList();
 
-        this.listView = (ListView) findViewById(R.id.about_listView);
+        ListView listView = (ListView) findViewById(R.id.about_listView);
 
         final AboutAdapter adapter = new AboutAdapter(AboutActivity.this, mList);
         listView.setAdapter(adapter);
@@ -31,7 +32,28 @@ public class AboutActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(AboutActivity.this, "Click item" + i, Toast.LENGTH_SHORT).show();
+                switch (i){
+                    case 0:
+                        Intent j = new Intent(Intent.ACTION_SEND);
+                        // j.setType("text/plain"); //模拟器请使用这行
+                        j.setType("message/rfc822"); // 真机上使用这行
+                        j.putExtra(Intent.EXTRA_EMAIL,
+                                new String[] { "wcb@pku.edu.cn" });
+                        j.putExtra(Intent.EXTRA_SUBJECT, "您发现的问题及建议");
+                        j.putExtra(Intent.EXTRA_TEXT, "我们很希望能得到您的建议！！！");
+                        startActivity(Intent.createChooser(j,
+                                "Select email application."));
+                      break;
+                    case 1:
+                        Toast.makeText(AboutActivity.this, "Click item" + 9, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(AboutActivity.this, "Click item" + 8, Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        Toast.makeText(AboutActivity.this, "Click item" + 7, Toast.LENGTH_SHORT).show();
+                        break;
+                }
             }
         });
     }
@@ -39,11 +61,32 @@ public class AboutActivity extends AppCompatActivity {
     /**
      * 初始化数据
      */
-    private void initList() {
-        mList.add("问题反馈");
-        mList.add("开发人员");
-        mList.add("功能介绍");
-        mList.add("Github Page");
+    private void getMenuList() {
+        mList.add(new AboutMenu(R.mipmap.icon_email_600,"问题反馈（请用email打开）"));
+        mList.add(new AboutMenu(R.mipmap.icon_member_980,"开发人员"));
+        mList.add(new AboutMenu(R.mipmap.icon_function_768,"功能介绍"));
+        mList.add(new AboutMenu(R.mipmap.icon_github_logo_1024,"Github Page"));
+    }
+
+    public class AboutMenu{
+        private int imageId;
+        private String menu;
+
+        AboutMenu(int imageId, String menu){
+            this.imageId = imageId;
+            this.menu = menu;
+        }
+
+        public String getMenu(){
+            return menu;
+        }
+        public void setMenu(){
+        }
+        public int getImageId(){
+            return imageId;
+        }
+        public void setImageId(){
+        }
     }
 }
 
--- app/src/main/java/edu_cn/pku/course/adapter/AboutAdapter.java
+++ app/src/main/java/edu_cn/pku/course/adapter/AboutAdapter.java
@@ -1,21 +1,25 @@
 package edu_cn.pku.course.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import edu_cn.pku.course.activities.AboutActivity;
 import edu_cn.pku.course.activities.R;
 
 public class AboutAdapter extends BaseAdapter {
     private Context mContext;
-    private List<String> mList = new ArrayList<>();
+    private List<AboutActivity.AboutMenu> mList = new ArrayList<>();
 
-    public AboutAdapter(Context context, List<String> list) {
+    public AboutAdapter(Context context, List<AboutActivity.AboutMenu> list) {
         mContext = context;
         mList = list;
     }
@@ -35,6 +39,7 @@ public class AboutAdapter extends BaseAdapter {
         return i;
     }
 
+    @SuppressLint("InflateParams")
     @Override
     public View getView(final int i, View view, ViewGroup viewGroup) {
         ViewHolder viewHolder = null;
@@ -42,14 +47,17 @@ public class AboutAdapter extends BaseAdapter {
             viewHolder = new ViewHolder();
             view = LayoutInflater.from(mContext).inflate(R.layout.item_about_list_view, null);
             viewHolder.mTextView = (TextView) view.findViewById(R.id.item_about_text);
+            viewHolder.mImageView = (ImageView) view.findViewById(R.id.item_about_image);
             view.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) view.getTag();
         }
-        viewHolder.mTextView.setText(mList.get(i));
+        viewHolder.mTextView.setText(mList.get(i).getMenu());
+        viewHolder.mImageView.setImageResource(mList.get(i).getImageId());
         return view;
     }
     class ViewHolder {
         TextView mTextView;
+        ImageView mImageView;
     }
 }
