4a3796e6d45a4a83c8cdb3716267824219646fdb
ZhaoRibo <1294396795@qq.com>
2018-12-07 16:01:04 +0800

    About
    我还是觉得红色好看，另外在about里添加了隐私协议，便于用户再次查看。。呃，然后没啥大的变化

--- app/src/main/java/edu_cn/pku/course/activities/AboutActivity.java
+++ app/src/main/java/edu_cn/pku/course/activities/AboutActivity.java
@@ -1,6 +1,8 @@
 package edu_cn.pku.course.activities;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -17,6 +19,7 @@ import android.widget.Toast;
 
 import com.r0adkll.slidr.Slidr;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -74,10 +77,13 @@ public class AboutActivity extends AppCompatActivity {
                         startActivity(it);
                         break;
                     case 2:
-                        Toast.makeText(AboutActivity.this, "别点了，再点功能也不会多的！\n不如点击“问题反馈”给予我们建议。", Toast.LENGTH_SHORT).show();
+                        Utils.showPrivacyPolicyDialog(AboutActivity.this);
                         break;
                     case 3:
-                        Toast.makeText(AboutActivity.this, "Bloom！\n这里有四只狗\n有意向可以选购", Toast.LENGTH_SHORT).show();
+                        ToastUtil.toast(AboutActivity.this,"别点了，再点功能也不会多的！\n不如点击“问题反馈”给予我们建议。");
+                        break;
+                    case 4:
+                        ToastUtil.toast(AboutActivity.this,"Bloom！\n这里有四只狗\n有意向可以选购");
                         break;
                 }
             }
@@ -101,9 +107,10 @@ public class AboutActivity extends AppCompatActivity {
      */
     private void getMenuList() {
         mList.add(new AboutMenu(R.mipmap.icon_email_600, "问题反馈", "Select email application to send an email to the developer.", null));
+        mList.add(new AboutMenu(R.mipmap.icon_protect_1,"隐私保护协议","Privacy Policy","如果您对app的隐私安全感到担忧，请您点击这里审阅我们的隐私保护协议。若仍有问题，请反馈给我们。"));
         mList.add(new AboutMenu(R.mipmap.icon_github_logo_1024, "Github Page", "Open source code on GitHub.", null));
         mList.add(new AboutMenu(R.mipmap.icon_function_768, "功能介绍", "Features", "此APP包含了course.pku.edu.cn的部分常用功能，例如公告、通知、课程列表及每个课程包含的内容等，另外添加了北京大学门户中空闲教室查询的功能，希望为寻找空闲教室的同学提供便利。\n"));
-        mList.add(new AboutMenu(R.mipmap.icon_member_980, "开发人员", "Developer", "本App由四个疯人院成员开发制作：\n\n" +
+        mList.add(new AboutMenu(R.mipmap.icon_member_980, "开发人员", "Developers", "本App由四个疯人院成员开发制作：\n\n" +
                 "·叶林楠\n    主要负责人\n        技术总监\n\n" +
                 "·Yinian\n    点子来源\n        吉祥物\n\n" +
                 "·杨老师歌迷\n    艺术总监\n\n" +
@@ -137,6 +144,29 @@ public class AboutActivity extends AppCompatActivity {
 
         public String getContent() {
             return content;
+        }
+    }
+
+    public static class ToastUtil {
+        /**
+         * Toast实例，用于对本页出现的所有Toast进行处理
+         */
+        private static Toast myToast;
+
+        /**
+         * 此处是一个封装的Toast方法，可以取消掉上一次未完成的，直接进行下一次Toast
+         * @param context context
+         * @param text 需要toast的内容
+         */
+        @SuppressLint("ShowToast")
+        public static void toast(Context context, String text){
+            if (myToast != null) {
+                myToast.cancel();
+                myToast=Toast.makeText(context,text,Toast.LENGTH_SHORT);
+            }else{
+                myToast=Toast.makeText(context,text,Toast.LENGTH_SHORT);
+            }
+            myToast.show();
         }
     }
 }
